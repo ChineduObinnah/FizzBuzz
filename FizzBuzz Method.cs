b5cfbc35c32ab61bf58f993a4f7dae7c0fc41943
Chinedu Obinnah~ <cobinnah@yahoo.com>
2023-06-08 23:00:56 -0500

    FizzBuzz Complete

---
+++ FizzBuzz Method.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fizzbuzz1
+{
+    public static class FizzBuzz_Method
+    {
+        public static string GetZero(int num)
+        {
+
+            
+            if (num % 5 == 0 && num % 3 == 0)
+            {
+                return $"{num}: fizzbuzz";
+
+            }
+
+            else if (num % 3 == 0)
+            {
+                return  $"{ num}: fizz";
+            }
+
+
+
+            else if (num % 5 == 0)
+            {
+                return $"{num}: buzz";
+
+            }
+
+
+            else
+            {
+                return $"{ num}: INVALID!!! Does not divide by 3 or 5 !";
+
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using Fizzbuzz1;
+
+      var numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+        
+        foreach( int number in numbers)
+{
+    Console.WriteLine(FizzBuzz_Method.GetZero(number));
+}
